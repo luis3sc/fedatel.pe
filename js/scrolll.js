a19dc0cd1f64b30d357244aaeacb2c3ca63268a1
Luis Enrique <33030471+luis3sc@users.noreply.github.com>
2022-09-21 01:08:51 -0500

    Add files via upload

---
+++ js/scrolll.js
@@ -0,0 +1,47 @@
+ // GENERAL SETTING
+ window.sr = ScrollReveal({ reset: true });
+    
+ // Custom Settings
+ sr.reveal('.top-a', {
+  origin: 'top', 
+  distance: '10px',
+  duration: 300,
+});
+ sr.reveal('.promocion', {
+   origin: 'top', 
+   distance: '10px',
+   duration: 500,
+ });
+ 
+ sr.reveal('.catalogo', { 
+   origin: 'top', 
+   distance: '20px',
+   duration: 800,
+ });
+ 
+ sr.reveal('.bene', { 
+    origin: 'top', 
+   distance: '30px',
+   duration: 1000,
+ });
+ 
+ sr.reveal('.soppporte', { 
+   origin: 'top', 
+   distance: '40px',
+   duration: 1300,
+ });
+ sr.reveal('.lft-a', { 
+   origin: 'left', 
+   distance: '40px',
+   duration: 1400,
+ });
+ sr.reveal('.rgt-a', { 
+   origin: 'right', 
+   distance: '40px',
+   duration: 1350,
+ });
+ sr.reveal('.contratar', { 
+   origin: 'top', 
+   distance: '40px',
+   duration: 1600,
+ });
